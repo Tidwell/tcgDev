176aa89ac55d01dff6119c81d9b8bd0834f1c114
Aaron Tidwell <aaron.tidwell@gmail.com>
2013-01-13 01:00:24 -0500

    first

---
+++ js/app.js
@@ -0,0 +1,42 @@
+(function() {
+	/*all navigation stuff to turn it into a single-page app*/
+	var $home;
+	var $digitalList;
+
+	var total;
+	var cnt;
+	var cb;
+	function hideAll(callback) {
+		cb = callback;
+		total = 2;
+		cnt = 0;
+		$home.fadeOut(count);
+		$digitalList.fadeOut(count);
+	}
+	function count() {
+		cnt++;
+		if (cnt === total) {
+			cb();
+		}
+	}
+
+	function changePage() {
+		var page = location.hash.replace('#','');
+		hideAll(function(){
+			$('.nav .active').removeClass('active');
+			$('.nav a[href="#'+page+'"]').parent().addClass('active');
+			$('#'+page).fadeIn();
+		});
+	}
+
+	function getElements() {
+		$home = $('#home');
+		$digitalList = $('#digital-list');
+	}
+
+	$(function(){
+		getElements();
+		$(window).hashchange(changePage);
+		$(window).hashchange();
+	});
+}());
